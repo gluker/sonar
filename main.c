2a96350a6b791d65e82a2daaabd3efae3063c7b1
gluker <glukerr@gmail.com>
2013-02-08 22:17:54 +0200

    The force is weak with this one

--- main.c
+++ main.c
@@ -12,58 +12,28 @@
 #include "driverlib/timer.h"
 #include "driverlib/pin_map.h"
 #include "driverlib/systick.h"
+#include "driverlib/uart.h"
 
 #define	SONAR_PORT	GPIO_PORTD_BASE
 #define TRIG_PIN	GPIO_PIN_1
 #define ECHO_PIN	GPIO_PIN_0
-#define CLK			80000000
-unsigned long xyu = 0;
+#define	SERVO_PIN	GPIO_PIN_2
 
-void Timer0AIntHandler(void){
-	static char flag = 0;
+unsigned long f_cpu = 0;
+unsigned long dist = 0;
 
-	if(flag){
-		GPIOPinWrite(SONAR_PORT,TRIG_PIN,0);
-		TimerIntDisable(TIMER0_BASE, TIMER_TIMA_MATCH);
-		TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-		flag = 0;
-	}else{
-		GPIOPinWrite(SONAR_PORT,TRIG_PIN,1);
-		TimerIntDisable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-		TimerIntEnable(TIMER0_BASE, TIMER_TIMA_MATCH);
-		flag = 1;
-	}
-}
-
-void GPIODIntHandler(void){
-
-	static unsigned long cnt = 0;
-	static int state = 0;
-
-	if (state){
-		cnt -= SysTickValueGet();
-		cnt = (cnt/80000);
-		GPIOIntTypeSet(SONAR_PORT, ECHO_PIN, GPIO_RISING_EDGE);
-		state = 0;
-
-	}else{
-		cnt = SysTickValueGet();
-		GPIOIntTypeSet(SONAR_PORT, ECHO_PIN, GPIO_FALLING_EDGE);
-		state = 1;
-	}
-
-
-
-}
 
 void hw_init(void){
 	SysCtlClockSet(SYSCTL_SYSDIV_2_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 
+	f_cpu = SysCtlClockGet();
 	SysTickPeriodSet(0xffffffff);
 	SysTickEnable();
+//	UARTStdioInit();
 
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
 	GPIODirModeSet(SONAR_PORT, TRIG_PIN, GPIO_DIR_MODE_OUT);
+	GPIODirModeSet(SONAR_PORT, SERVO_PIN, GPIO_DIR_MODE_OUT);
 
 	GPIOPinTypeGPIOInput(SONAR_PORT, ECHO_PIN);
 	GPIOIntTypeSet(SONAR_PORT, ECHO_PIN, GPIO_RISING_EDGE);
@@ -73,8 +43,8 @@ void hw_init(void){
 	//Timer configuration
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
 	TimerConfigure(TIMER0_BASE, TIMER_CFG_A_PERIODIC);
-	const long timer_match = (CLK/1000000)*10;
-	const long timer_out = (CLK/1000)*100;
+	const long timer_match = (f_cpu/1000000)*10;
+	const long timer_out = (f_cpu/1000)*80;
 	TimerLoadSet(TIMER0_BASE, TIMER_A, timer_out);
 	TimerMatchSet(TIMER0_BASE, TIMER_A, timer_match);
 	TimerEnable(TIMER0_BASE, TIMER_A);
@@ -95,4 +65,44 @@ void main(void) {
 	}
 
 }
+
+void Timer0AIntHandler(void){
+	static char flag = 0;
+
+	if(flag){
+		GPIOPinWrite(SONAR_PORT,TRIG_PIN,0);
+		TimerIntDisable(TIMER0_BASE, TIMER_TIMA_MATCH);
+		TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+		flag = 0;
+	}else{
+		GPIOPinWrite(SONAR_PORT,TRIG_PIN,1);
+		TimerIntDisable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+		TimerIntEnable(TIMER0_BASE, TIMER_TIMA_MATCH);
+		flag = 1;
+	}
+}
+
+
+
+
+void GPIODIntHandler(void){
+
+	static unsigned long cnt = 0;
+	static int state = 0;
+
+	if (state){
+
+		//dist = (f_cpu/(SysTickValueGet()-cnt))*58;
+		dist = SysTickValueGet();
+		dist = cnt - dist;
+		GPIOIntTypeSet(SONAR_PORT, ECHO_PIN, GPIO_RISING_EDGE);
+		state = 0;
+
+	}else{
+		cnt = SysTickValueGet();
+		GPIOIntTypeSet(SONAR_PORT, ECHO_PIN, GPIO_FALLING_EDGE);
+		state = 1;
+	}
+}
+
 
